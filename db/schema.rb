d10826d61934403f5bc25206a22a1d8a6480e1ff
Lisset Cuevas Juarez <lisset.cuevas@Lissets-MacBook-Pro.local>
2022-01-21 13:44:52 -0600

    Create abilities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_014715) do
+ActiveRecord::Schema.define(version: 2022_01_21_193652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "generation"
+    t.text "effect"
+    t.text "short_effect"
+    t.integer "api_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name", null: false
