2fca08498ba7a76db30626a9a0d54c00bce9863c
Lisset Cuevas Juarez <lisset.cuevas@Lissets-MacBook-Pro.local>
2022-01-26 12:03:40 -0600

    User Authentication and new pokemon form including associations

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -10,13 +10,24 @@ class PokemonController < ApplicationController
 
   def new
     @pokemon = Pokemon.new
+    @types = Type.all
+    @abilities = Ability.all
   end
 
   def create
+    @types = Type.all
+    @abilities = Ability.all
     @pokemon = Pokemon.new(pokemon_params)
+
+    @pokemon.types << find_types_by_name(params[:pokemon][:types])
+    @pokemon.abilities << find_abilities_by_name(params[:pokemon][:abilities])
+
+    @pokemon.user_id = current_user.id
+
     if @pokemon.save
       redirect_to(pokemon_index_path)
     else
+      flash[:error] = "Error creating pokemon, add it "
       render('new')
     end
   end
@@ -39,6 +50,24 @@ class PokemonController < ApplicationController
     @pokemon = Pokemon.find(params[:id])
     @pokemon.destroy
     redirect_to(pokemon_index_path, status: 303)
+  end
+
+  def find_types_by_name(types)
+    types_objects = []
+    types.reject(&:empty?).each do |type_name|
+      type = Type.find_by_name(type_name)
+      types_objects.push(type)
+    end
+    types_objects
+  end
+
+  def find_abilities_by_name(abilities)
+    abilities_objects = []
+    abilities.reject(&:empty?).each do |ability_name|
+      ability = Ability.find_by_name(ability_name)
+      abilities_objects.push(ability)
+    end
+    abilities_objects
   end
 
   private
